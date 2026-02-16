c1fd9245ee33c3ba23a9894efd2f0d7c004af764
unknown <melanyrm2507@gmail.com>
2026-02-15 23:52:37 -0400

    Agregamos getor de estados Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tasks_to_do_app/core/theme/app_theme.dart';
 import 'package:tasks_to_do_app/features/auth/presentation/pages/login_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: const MainApp()));
 }
 
 class MainApp extends StatelessWidget {
@@ -15,7 +16,7 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       //darkTheme: AppTheme.darkTheme,
-      home: const LoginPage(),
+      home: LoginPage(),
     );
   }
 }
